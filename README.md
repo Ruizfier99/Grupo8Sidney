bc5e8fae7a122df8032d77d6e0536773e0543592
nacaicedol <nacaicedol@unal.edu.co>
2021-09-11 02:58:54 -0500

    Version NCL

--- README.md
+++
@@ -1,9 +0,0 @@
-# Grupo8Sidney
-App Sidney
-
-### Integrantes:
-* Ivan Enrique	Peñaranda Ortiz
-* Jhon Emerson	Jaimes Nova
-* Julian Alberto 	Torres Montoya
-* Andres David 	Ruiz Fierro
-* Nicolás	Caicedo León
---
+++ app/src/main/java/com/example/grupo8sidney/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.grupo8sidney
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
