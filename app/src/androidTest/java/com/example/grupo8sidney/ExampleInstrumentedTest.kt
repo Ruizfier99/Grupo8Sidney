5f93647ea33eeef4b05b9b940cf62fdfded16ce9
Julian Alberto Torres Montoya <jtorresm@dian.gov.co>
2021-09-15 19:00:40 -0500

    prueba test

--- app/src/androidTest/java/com/example/grupo8sidney/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/grupo8sidney/ExampleInstrumentedTest.kt
@@ -12,6 +12,7 @@ import org.junit.Assert.*
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
+ * test main
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
